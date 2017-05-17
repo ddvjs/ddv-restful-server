6381cc04b99e43ff2f78ffd4236442d05793d2e7
hua <yuchonghua@163.com>
2017-05-17 21:46:08 +0800

    fixbug

--- index.js
+++ index.js
@@ -9,16 +9,22 @@ var isServerStart = false
 worker.serverStart = function serverStart (options, siteConfigFile) {
   if (isServerStart) {
     logger.error('Please do not start many times')
+    process.exit(-1)
     return
   }
   isServerStart = true
   if (!fs.existsSync(options.appPath)) {
     logger.error('Options.appPath does not exist, please check')
+    logger.error(options.appPath)
+    process.exit(-1)
     return
   }
   require('ddv-worker-express-ws')(options)
   // 运行app
-  require(options.appPath)
+  let fn = require(options.appPath)
+  if (typeof fn === 'function') {
+    fn(options, siteConfigFile)
+  }
   // 监听服务 - Listen the server
   worker.updateServerConf({
     defaultListen: options.defaultListen,
